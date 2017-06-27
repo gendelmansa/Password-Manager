7a43e023786a8d2e8015560158cae207d3a17507
gendelmansa <gendelmansa@gmail.com>
2017-06-27 15:39:05 -0500

    first upload

---
+++ passwordmanager/src/app/components/login/login.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './components/login/login.component.html',
+})
+export class LoginComponent  { name = 'Angular'; }
