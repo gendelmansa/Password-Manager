38db26d7b541abf7b1fd67521788687354456f4f
gendelmansa <gendelmansa@gmail.com>
2017-07-05 16:40:29 -0500

    with routing, home component, and a little bit of design of the register page

--- passwordmanager/src/app/app.module.ts
+++ passwordmanager/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent }  from './app.component';
 
@@ -8,9 +9,15 @@ import { LoginComponent } from './components/login/login.component'
 import { NavbarComponent } from './components/navbar/navbar.component'
 import { RegisterComponent } from './components/register/register.component'
 
+const appRoutes: Routes = [
+  { path:'register', component: RegisterComponent},
+  { path:'login', component: LoginComponent}
+]
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ RouterModule.forRoot(appRoutes), BrowserModule ],
   declarations: [ AppComponent, LoginComponent, NavbarComponent, RegisterComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+ }
