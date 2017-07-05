85e18310a67f637da22e5d1c7de6b58a4c21523a
gendelmansa <gendelmansa@gmail.com>
2017-07-05 11:51:08 -0500

    second commit and fix on npm start

--- passwordmanager/src/app/app.module.ts
+++ passwordmanager/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent }  from './app.component';
+
 import { LoginComponent } from './components/login/login.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { RegisterComponent } from './components/register/register.component'
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, LoginComponent, NavbarComponent, RegisterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- passwordmanager/src/app/components/login/login.component.ts
+++ passwordmanager/src/app/components/login/login.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
+  moduleId: module.id,
   selector: 'login',
-  templateUrl: './components/login/login.component.html',
+  templateUrl: './login.component.html',
 })
-export class LoginComponent  { name = 'Angular'; }
+export class LoginComponent  { 
+ 
+  newprofile = false;
+  
+  toggleregister() {
+    this.newprofile=!this.newprofile
+  }
+}
