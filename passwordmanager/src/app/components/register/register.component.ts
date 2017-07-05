fd75ea3622781ee682b2eba658ee62b06c01cf13
gendelmansa <gendelmansa@gmail.com>
2017-07-05 11:51:34 -0500

    commit

---
+++ passwordmanager/src/app/components/register/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId:module.id,
+  selector: 'register',
+  templateUrl: './register.component.html',
+})
+
+export class RegisterComponent  { }
